893f23538d75c09bb63673abc682a4da799039c6
tang boqi <tboqi301709@gmail.com>
2015-01-13 16:09:49 +0800

    a

--- php/khn/khn3/khn3blog_number1g.com/application/bootstrap.php
+++ php/khn/khn3/khn3blog_number1g.com/application/bootstrap.php
@@ -80,7 +80,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  caching     enable or disable internal caching                 FALSE
  */
 Kohana::init(array(
-	'base_url'   => '/',
+	'base_url'   => '/khn3blog/',
 	'index_file'   => '',
 ));
 
